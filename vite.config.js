23bac46255812bb3ded05eb75487f0c51d4c3648
srinitech-ai <psrinivasuludev@gmail.com>
2025-10-26 11:21:00 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // ✅ This is critical for S3 or relative-path hosting
+})
